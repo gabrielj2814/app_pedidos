eb6c586db2638cf29a5669a10df66c6751f5fa0e
gabriel valera <hashiramakage@gmail.com>
2022-10-19 22:57:21 -0400

    agregando funcion de crear, consultar todo y por id pero sin validar

--- backend/src/controllers/plato.ts
+++ backend/src/controllers/plato.ts
@@ -5,19 +5,60 @@ import respuesta from "../types/respuesta"
 
 let PlatoControlador ={
 
-    crearPlato: async (req:Request,res:Response) => {
+    crear: async (req:Request,res:Response) => {
         let {body} = req
         let {...plato} = body
-        // console.log("datos plato => ", plato)
         let platoNuevo= new Plato(plato)
-        await platoNuevo.save()
-        let platos =await Plato.find()
-        console.log("listar datos => ",platos)
-        res.status(200).json({mensaje:"ok"})
+        let platoCreado = await platoNuevo.save()
+        // let platos =await Plato.find()
+        // console.log("listar datos => ",platos)
+        res.status(200).json(platoCreado)
+    },
+    
+    consultarTodo:async (req:Request,res:Response) => {
+        let platos = await Plato.find()
+        res.status(200).json(platos)
+    },
+    
+    consultarPorId:(req:Request,res:Response) => {
+        let {idPlato} = req.params
+        Plato.findById(idPlato)
+        .then(plato => {
+            if(plato){
+                res.status(200).json(plato)
+            }
+            else{
+                res.status(404).json({msj:"no encontrado"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    },
 
-        
+    consultarPorNombre:(req:Request,res:Response) => {
+        let {nombre} = req.params
+        console.log(nombre)
+        // TODO: INVESTIGAR COMO HACER BUSQUEDA POR TEXTO
+        // Plato.ensureIndexes({nombre:"text"})
+        // Plato.find({
+        //     $text:{
+        //         $search: '\"plato\"',
+        //         $caseSensitive: false,
+        //     }
+        // })
+        // .then(plato => {
+        //     if(plato){
+        //         res.status(200).json(plato)
+        //     }
+        //     else{
+        //         res.status(404).json({msj:"no encontrado"})
+        //     }
+        // })
+        // .catch(err => {
+        //     res.status(500).json(err)
+        // })
     }
-
 }
 
 export default PlatoControlador
--- backend/src/v1/routes/plato/index.ts
+++ backend/src/v1/routes/plato/index.ts
@@ -7,6 +7,12 @@ route.get("/",(req:Request,res:Response) => {
     res.status(200).send(`url => ${req.baseUrl}`)
 })
 
-route.post("/crear",PlatoControlador.crearPlato)
+route.post("/crear",PlatoControlador.crear)
+route.get("/consultar/todo",PlatoControlador.consultarTodo)
+route.get("/consultar/id/:idPlato",PlatoControlador.consultarPorId)
+// route.get("/consultar/nombre-plato/:nombre",PlatoControlador.consultarPorNombre)
+
+// route.put("/actualizar/:id",PlatoControlador.crearPlato)
+// route.delete("/eliminar/:id",PlatoControlador.crearPlato)
 
 export default route
